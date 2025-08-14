fe9bc59b6abdd90f676d86ed070a703029484fbc
Bengi Anıl <bengianil34@gmail.com>
2025-08-14 23:15:18 +0300

    Created project files

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Bengi Anıl on 14.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
